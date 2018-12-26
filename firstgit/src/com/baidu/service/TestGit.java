848775dd36b396e92ac1258cbe3c983fbac82551
weijianjun <1281514787@qq.com>
2018-12-26 20:13:46 +0800

    将firstgit加入版本库

---
+++ firstgit/src/com/baidu/service/TestGit.java
@@ -0,0 +1,8 @@
+package com.baidu.service;
+
+public class TestGit {
+    public static void main(String[] args){
+        System.out.println("训练java");
+        System.out.println("训练java");
+    }
+}

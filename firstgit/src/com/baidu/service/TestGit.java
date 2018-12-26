5537966fc405f9fa8103c157bc4e595d450d59f6
weijianjun <1281514787@qq.com>
2018-12-26 20:39:09 +0800

    将firstrep修改添加后再次上传

--- firstgit/src/com/baidu/service/TestGit.java
+++ firstgit/src/com/baidu/service/TestGit.java
@@ -4,8 +4,12 @@ public class TestGit {
     public static void main(String[] args){
         System.out.println("训练java");
         System.out.println("训练java");
+        //这是注解
         int i=1;
         i+=10;
 
+    }
+    public void method(){
+        System.out.println("这是一个方法");
     }
 }
